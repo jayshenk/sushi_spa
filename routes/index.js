cb4b41cce76a0ac8177607e65c4b7bfa415b963b
Jay Shenk <jay.shenk@gmail.com>
2017-04-29 16:13:51 -0400

    Setup backend index route and some sample json data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var path = require("path");
+var fs = require("fs");
+var file_path = path.resolve(path.dirname(__dirname), "data/menu_items.json");
 var router = express.Router();
 
+function getMenuItems() {
+  return JSON.parse(fs.readFileSync(file_path, "utf8"));
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/index.html', function(req, res, next) {
+  res.render('index', {
+    menuItems: getMenuItems()
+  });
 });
 
 module.exports = router;
